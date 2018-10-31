ff827534766a6345ea4bc6f3ed1de56c032dcbde
MohsAkhtar <mohs.akhtar@gmail.com>
2018-10-31 10:51:41 +0000

    Updates

---
+++ precache-manifest.28dd59111a66127c43566a8adceeb094.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da7fd94ef675fa8a2314",
+    "url": "/project-react-spotify-api/static/js/runtime~main.da7fd94e.js"
+  },
+  {
+    "revision": "a31ad67bce649c60b433",
+    "url": "/project-react-spotify-api/static/js/main.a31ad67b.chunk.js"
+  },
+  {
+    "revision": "83b3f9b8655729265301",
+    "url": "/project-react-spotify-api/static/js/1.83b3f9b8.chunk.js"
+  },
+  {
+    "revision": "a31ad67bce649c60b433",
+    "url": "/project-react-spotify-api/static/css/main.03a32b58.chunk.css"
+  },
+  {
+    "revision": "1564d5bafa10e4dc3f42d2e2b95d403e",
+    "url": "/project-react-spotify-api/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/project-react-spotify-api/precache-manifest.28dd59111a66127c43566a8adceeb094.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/project-react-spotify-api/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
